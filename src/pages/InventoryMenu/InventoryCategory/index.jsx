6976cbbe7ea544f2b1c5aa244b9d6e42e646c40e
ChairHomard45 <delepaut.clement@gmail.com>
2024-10-09 19:26:50 +0200

    -- Second Commit -- Minor changes in 'InventoryCategory':  - Added Proptypes for 'category'

--- src/pages/InventoryMenu/InventoryCategory/index.jsx
+++ src/pages/InventoryMenu/InventoryCategory/index.jsx
@@ -1,8 +1,13 @@
+import PropTypes from 'prop-types'
 
 function InventoryCategory({category}) {
     return (
         <h1> InventoryCategory { category }</h1>
     )
 }
+
+InventoryCategory.propTypes = {
+    category: PropTypes.string,
+};
 
 export default InventoryCategory
